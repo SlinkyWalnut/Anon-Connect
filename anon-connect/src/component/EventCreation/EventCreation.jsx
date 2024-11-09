60793c9df3e728bf307e0f02191a7b510acee42e
TheAlienSeb <93489617+TheAlienSeb@users.noreply.github.com>
2024-11-09 02:37:36 -0500

    shitty code

--- anon-connect/src/component/EventCreation/EventCreation.jsx
+++ anon-connect/src/component/EventCreation/EventCreation.jsx
@@ -1,9 +1,43 @@
 import React from 'react'
 
 function EventCreation() {
+
+
+
+
+
+
+
+
+
+
   return (
     <div>
-      Event Creation
+        <h1>Event Creation</h1>
+        <form>
+            <label for='eventName' >Event Name</label>
+            <input type="text" id="eventName" name="eventName"></input>
+            <label for='Location' >Event Location</label>
+            <input type="text" id="eventLocation" name="eventLocation"></input>
+            <label for='eventDate' >Event Date</label>
+            <input type="date" id="eventDate" name="eventDate"></input>
+            <label for='eventTime' >Event Duration</label>
+            <input type="date" id="eventDate" name="eventDate"></input>
+            <label for='eventDuration' >Event Duration</label>
+            <input type="text" id="eventDuration" name="eventDuration"></input>
+            <label for='eventDescription' >Event Description</label>
+            <input type="text" id="eventDiscription" name="eventDescription"></input>
+            <label for='eventSponsors' >Event Sponsors</label>
+            <input type="text" id="eventSponsors" name="eventSponsors"></input>
+            <label for='eventSpeakers' >Event Speakers</label>
+            <input type="text" id="eventSpeakers" name="eventSpeakers"></input>
+            <label for='eventTags' >Event Tags</label>
+            <input type="text" id="eventTags" name="eventTags"></input>
+            <input type="submit">Submit</input>
+        </form>
+
+
+
     </div>
   )
 }
--- anon-connect/src/component/EventDisplay/EventThread/EventThread.jsx
+++ anon-connect/src/component/EventDisplay/EventThread/EventThread.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 
+import EventItem from '../EventItem/EventItem'
+
 function EventThread() {
+  
+  
+  
+  
+  
   return (
     <div>
       Event Thread
+      <div style={{display:flex, justifyContent:center, alignItems:center}}>
+        < EventItem 
+        EventItem = {{eventTitle:"Druggies stop by", Description:"Here we help druggies go cold turkey by locking them up", Location:"My basement"}} 
+        />
+
+
+      </div>
+
+
     </div>
   )
 }
