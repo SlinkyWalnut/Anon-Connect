05af9dc6a96231d9eef38bdc62ca9deffee0764b
Will Liu <willliu2001@gmail.com>
2024-11-09 02:45:16 -0500

    Event item thread update

--- anon-connect/src/component/EventDisplay/EventThread/EventThread.jsx
+++ anon-connect/src/component/EventDisplay/EventThread/EventThread.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
-function EventThread() {
+function EventThread({description, location}) {
   return (
-    <div>
-      Event Thread
+    <div className='border my-2 border-black rounded p-2'>
+      <div><h2 className='font-bold text-xl mb-4'>Event Title</h2></div>
+      <p className='my-2'>{description}</p>
+      <p>Location: {location}</p>
     </div>
   )
 }
