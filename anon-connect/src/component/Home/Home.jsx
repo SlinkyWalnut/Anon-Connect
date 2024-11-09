26c9d33b59b6fe73fd8af93b929183943c2e8c60
Will Liu <willliu2001@gmail.com>
2024-11-09 02:22:18 -0500

    Updated Home and its buttons

--- anon-connect/src/component/Home/Home.jsx
+++ anon-connect/src/component/Home/Home.jsx
@@ -1,15 +1,24 @@
-import React from 'react'
+import React, {useState} from 'react'
+import "./Home.css"
+import EventDisplay from '../EventDisplay/EventDisplay';
+
+
 
 function Home() {
+  const [eventsClicked, setEventsClicked] = useState(false);
+  const handleEventsClicked = () => {
+    setEventsClicked(true);
+  }
   return (
-    <div>
+    (!eventsClicked ? <div>
       <div>
-        <h2>About Us</h2>
+        <h2 className='text-5xl p-4'>About Us</h2>
       </div>
-      <div>
-        
+      <div className='flex justify-center'>
+        <button className='rounded-lg mr-4 bg-blue-700 text-white p-2 hover:opacity-70'>Organizer</button>
+        <button onClick={handleEventsClicked} className='rounded-lg mr-4 bg-blue-700 text-white p-2 hover:opacity-70'>Events</button>
       </div>
-    </div>
+    </div> : <EventDisplay />)
   )
 }
 
