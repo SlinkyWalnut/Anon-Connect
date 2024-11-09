b3e69b8ff7a7556085bff144c6eefea148a7861d
aleksShif <sasha.shifrina05@gmail.com>
2024-11-09 03:09:13 -0500

    reinstalled npm, added to app

--- anon-connect/src/App.js
+++ anon-connect/src/App.js
@@ -1,6 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './Home/Home';
+import { createContext } from 'react';
+
+const UserContext = createContext(); 
+
+function LoginCheck({children}) {
+  const [isLoggedIn, isLoggedInSet] = useState(true); 
+ 
+  return (
+    <UserContext.Provider value={{ isLoggedIn }}>{children}</UserContext.Provider>
+  );
+}
 
 function App() {
   return (
